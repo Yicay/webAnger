81ce702a70e35e03c9e17252379839f1bf715eb6
Yicay <47560554+Yicay@users.noreply.github.com>
2019-03-17 14:07:04 +0800

    web

---
+++ js/sh.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?d1c9e87c25d29d1ca2317c828772b0c2";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
